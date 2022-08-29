cc7e9d8e3595ca0dcc47407c97ade7bd45353b00
VncntRnld <93307769+VncntRnld@users.noreply.github.com>
2022-08-29 19:00:43 +0700

    Upload UG_01

---
+++ 71210692_01.py
@@ -0,0 +1,32 @@
+
+def kalkulator():
+    print("Ketik Q jika sudah selesai.")
+    print("Format : a + b")
+    n = input("Masukkan perhitungan : ")
+
+    if n == "Q":
+        global status
+        status = False
+        return
+
+    n = n.split()
+
+    if n[1] == "+":
+        return int(n[0])+int(n[2])
+    elif n[1] == "-":
+        return int(n[0])-int(n[2])
+    elif n[1] == "*":
+        return int(n[0])*int(n[2])
+    elif n[1] == "/":
+        return int(n[0])/int(n[2])
+    else:
+        return "Invalid input"
+
+
+status = True
+while status == True:
+    print()
+    x = kalkulator()
+
+    if x != None:
+        print("Hasilnya : {}".format(x)) 
